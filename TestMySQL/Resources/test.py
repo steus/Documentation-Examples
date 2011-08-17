598d79400f3f0b40cf750fb47678b19d69293b62
Alan DuBoff <aland@canoe.local>
2011-08-16 18:42:14 -0700

    add a test example to use MySQL from Python

---
+++ TestMySQL/Resources/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import fnmatch
+import glob
+import MySQLdb
+   
+use_my_python_data("**** from Python ****");
+
+db = MySQLdb.connect (host = 'localhost', user = 'root', passwd = 'app123', db = 'test')
+
+cursor = db.cursor();
+cursor.execute("select firstname,lastname, last_updated from users order by lastname");
+rows = cursor.fetchall();
+for row in rows:
+	use_my_python_data(row[0] + ' ' + row[1])
+
+cursor.close();
+db.close();
+sys.exit(0);
