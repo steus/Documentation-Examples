06c442ed0ef9055d16e1f0fba20795b84dcbba2e
Tony Lukasavage <anthony.lukasavage@gmail.com>
2011-10-13 19:45:23 -0300

    added monkeypatch comment

--- CallbacksAreYourFriend/Resources/app.js
+++ CallbacksAreYourFriend/Resources/app.js
@@ -1,3 +1,11 @@
+/*
+add a monkey-patched "require" function to the global scope (global object).
+It is smarter in two ways:
+- It only loads a module once
+- If the exports object contains a function matching the module base name, return that
+  value from "require" - this is a bit of sugar added because Titanium's require implementation
+  does not allow you to replace the "exports" object directly
+*/
 require('lib/require').monkeypatch(this);
 
 //add a single variable to the global scope to which we may choose to
